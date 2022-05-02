0c337558cae0aaf13925aea33b7ad136c957e6a5
tejinder-sharma <tejinderwebdev@gmail.com>
2022-05-02 16:12:25 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'hero-pattern':
-          "linear-gradient(to right, rgba(168, 85, 247, 0.6), rgba(168, 85, 247, 0.1)), url('https://images.pexels.com/photos/6238050/pexels-photo-6238050.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')",
+          "linear-gradient(to right, rgba(168, 85, 247, 0.7), rgba(168, 85, 247, 0.1)), url('https://images.pexels.com/photos/6238050/pexels-photo-6238050.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')",
       },
       fontFamily: {
         Montserrat: ['Montserrat', 'sans-serif'],
